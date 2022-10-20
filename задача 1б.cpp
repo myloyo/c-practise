ffa80522fe35eec0953b6f367ce5d8bc1beb7661
myloyo <76838151+myloyo@users.noreply.github.com>
2022-10-20 20:16:32 +0400

    Add files via upload

---
+++ задача 1б.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	cin >> a;
+	switch (a) {
+	case 1:
+		cout << "налево" << endl; break;
+	case 2:
+		cout << "вверх" << endl; break;
+	case 3:
+		cout << "вправо"<< endl; break;
+	case 4:
+		cout << "вниз" << endl; break;
+	}
+	if (a == 1 || a == 3) {
+		cout << "горизонтальное движение";
+	}
+	else {
+		cout << "вертикальное движение";
+	}
+	return 0;
+}
