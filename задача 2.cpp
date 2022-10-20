c7077421b2a4ae6088a31f07f42c68ba108448cc
myloyo <76838151+myloyo@users.noreply.github.com>
2022-10-20 20:47:10 +0400

    Add files via upload

---
+++ задача 2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int a, b, c, d, x;
+	cin >> a >> b >> c;
+	setlocale(LC_ALL, "Russian");
+	d = pow(b, 2) - 4 * a * c;
+	if (d > 0) {
+		cout << "два корня";
+	}
+	else if (d == 0) {
+		cout << "один корень";
+	}
+	else if (d < 0) {
+		cout << "корней нет";
+	}
+	return 0;
+}
